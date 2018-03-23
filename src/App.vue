102532f2d2bee13c10c7cc3fb6af1561f75cda76
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-23 19:35:28 +0000

    Add toggle method

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,25 @@
     <p @click="countUp" class="event-button">Count Event</p>
     <p class="counter">Count is <span class="count-num">{{ counter }}</span>.</p>
 
+    <p @click="toggle" class="event-button">Toggle Links</p>
     <!-- vue links -->
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div v-if="showLinks">
+        <h2>Essential Links</h2>
+        <ul>
+          <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+          <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+          <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+          <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+        </ul>
+        <h2>Ecosystem</h2>
+        <ul>
+          <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+          <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+          <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+          <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+        </ul>
+    </div>
+
   </div>
 </template>
 
@@ -36,12 +40,17 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       url: 'https://github.com/NAKKA-K',
-      counter: 1
+      counter: 1,
+      showLinks: false
     }
   },
   methods: {
     countUp() {
       this.counter++;
+    },
+
+    toggle() {
+      this.showLinks = !this.showLinks
     }
   }
 }
