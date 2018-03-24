d487f7925a8290234125ce797074241b6732fd98
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-24 07:01:44 +0000

    Add POST view

---
+++ src/Post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>ID: {{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: "Post ID"
+      }
+    }
+  }
+</script>
