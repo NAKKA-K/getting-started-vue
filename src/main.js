d8a81edffe8028f90ad89d6629792c7c66903381
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-24 06:28:24 +0000

    Add router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+
+const Foo = { template: '<div>Foo</div>' }
+import ExampleComponent from './Example.vue'
+
+Vue.use(Router)
+var router = new Router({
+  routes: [
+    { path: '/example', component: ExampleComponent },
+    { path: '/foo', component: Foo }
+  ]
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
